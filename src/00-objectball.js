c807d5de03ca0b6d594eb7659937ca1f4220f1cf
Hasan <Hasanaldhahi3@gmail.com>
2022-04-24 02:12:35 +0300

    assignment finsihed

---
+++ src/00-objectball.js
@@ -0,0 +1,16 @@
+const gameObject = () => {
+
+    return {
+        home: {
+            teamName: ,
+            colors: ,
+            players: 
+        }
+        away: {
+
+            teamName: ,
+            colors: ,
+            players: 
+        }
+    }
+}
